35e420b28ad64f45f56bca7ea9641a5b9012d4be
Aurélien Joro Randriananaty <randriananaty@gmail.com>
2023-06-18 11:43:58 +0300

    Wip models and adding Migration database

---
+++ Priority.Matrix.Manager/Migrations/20230618084121_DatabaseCreated.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Priority.Matrix.Manager.Migrations
+{
+    public partial class DatabaseCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    CategoryCode = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskPriorities",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskTitre = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    TaskDescription = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    TaskCreatedBy = table.Column<int>(type: "int", nullable: false),
+                    TaskToSee = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Hour = table.Column<int>(type: "int", nullable: false),
+                    TaskStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskPriorities", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_TaskPriorities_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskPriorities_CategoryID",
+                table: "TaskPriorities",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskPriorities");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+	public class RepositoryContext : DbContext
+	{
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TaskPriority>? TaskPriorities { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    //base.OnModelCreating(modelBuilder);
+        //    //modelBuilder.Entity<TaskPriority>().HasKey(x => x.Id);
+        //    //modelBuilder.Entity<Category>().HasKey(x => x.Id);
+        //}
+    }
+}
